65dc9558c78ecad4b84710a3d38d0c926e5c2d60
J-J-delaTorre <62063183+J-J-delaTorre@users.noreply.github.com>
2023-03-24 09:50:36 -0400

    Update Main_Code.ino

--- GoShopMainCode/Main_Code.ino
+++ GoShopMainCode/Main_Code.ino
@@ -1,35 +1,32 @@
-//
-// begin license header
-//
-// This file is part of Pixy CMUcam5 or "Pixy" for short
-//
-// All Pixy source code is provided under the terms of the
-// GNU General Public License v2 (http://www.gnu.org/licenses/gpl-2.0.html).
-// Those wishing to use Pixy source code, software and/or
-// technologies under different licensing terms should contact us at
-// cmucam@cs.cmu.edu. Such licensing terms are available for
-// all portions of the Pixy codebase presented here.
-//
-// end license header
-//
-
+#include <Adafruit_PWMServoDriver.h>
+#include <DigiPotX9Cxxx.h>
 #include <Pixy2.h>
 #include <PIDLoop.h>
-const int pwPin = 7;
 
-//variables needed to store values
+#define SERVO_FREQ 50   //  Analog servos run at ~50 Hz updates
 
+//Variables needed to store values
+const int USPin = 7;                    //Pin for Ultrasonic Sensor
 long pulse, inches, cm;
-
+int shieldPin=analogRead(A0);
+// Initialize Servo Shield Methods
+Adafruit_PWMServoDriver servoShield = Adafruit_PWMServoDriver();
+// Potentiometer Setup
+//DigiPot digiPot(2,3,4);                 //*****incomplete setup as of now*******
 Pixy2 pixy;
 PIDLoop panLoop(1200, 0, 1200, true);   // changes servo track speed
 PIDLoop tiltLoop(800, 0, 800, true);    // changes servo track speed
+
+
+
 
 void setup()
 {
   Serial.begin(57600);
   Serial.print("Starting...\n");
-  
+  // Initialize Servo Shield
+  servoShield.begin();
+  servoShield.setPWMFreq(SERVO_FREQ);
   // We need to initialize the pixy object
   pixy.init();
   // Use color connected components program for the pan tilt to track
@@ -42,62 +39,59 @@ void loop()
   int j;
   char buf[64];
   int32_t panOffset, tiltOffset;
-
-  //SENSOR CODE
-  pinMode(pwPin, INPUT);
+  
+  //UltraSonic SENSOR CODE
+  pinMode(USPin, INPUT);
   //Used to read in the pulse that is being sent by the MaxSonar device.
   //Pulse Width representation with a scale factor of 147 uS per Inch.
-  pulse = pulseIn(pwPin, HIGH);
+  pulse = pulseIn(USPin, HIGH);
   //147uS per inch
   inches = pulse / 147;
   //change inches to centimetres
   cm = inches * 2.54;
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
+ /* Serial.print(inches);                      //Serial Print for verification of sensor data
+  Serial.print("in, ");                      //Serial Print for verification of sensor data
+  Serial.print(cm);                          //Serial Print for verification of sensor data
+  Serial.print("cm");                        //Serial Print for verification of sensor data
+  Serial.println();                          //Serial Print for verification of sensor data
+ */
+  Serial.print(shieldPin);
+  if ((inches > 12) && (inches < 42)){
+    //PIXY Pan-Tilt CODE
+    // get active blocks from Pixy
+    pixy.ccc.getBlocks();
  
- if ((inches > 12) && (inches < 42))
- {
-
- 
-  //PIXY CODE
-  // get active blocks from Pixy
-  pixy.ccc.getBlocks();
- 
-  if (pixy.ccc.numBlocks)
-  {        
-    i++;
+    if (pixy.ccc.numBlocks)
+    {        
+      i++;
     
-    if (i%60==0)
-      Serial.println(i);  
+      if (i%60==0)
+        Serial.println(i);  
    
-    // calculate pan and tilt "errors" with respect to first object (blocks[0]),
-    // which is the biggest object (they are sorted by size).  
-    panOffset = (int32_t)pixy.frameWidth/2 - (int32_t)pixy.ccc.blocks[0].m_x;
-    tiltOffset = (int32_t)pixy.ccc.blocks[0].m_y - (int32_t)pixy.frameHeight/2;  
+      // calculate pan and tilt "errors" with respect to first object (blocks[0]),
+      // which is the biggest object (they are sorted by size).  
+      panOffset = (int32_t)pixy.frameWidth/2 - (int32_t)pixy.ccc.blocks[0].m_x;
+      tiltOffset = (int32_t)pixy.ccc.blocks[0].m_y - (int32_t)pixy.frameHeight/2;  
  
-    // update loops
-    panLoop.update(panOffset);
-    tiltLoop.update(tiltOffset);
+      // update loops
+      panLoop.update(panOffset);
+      tiltLoop.update(tiltOffset);
  
-    // set pan and tilt servos  
-    pixy.setServos(panLoop.m_command, tiltLoop.m_command);
+      // set pan and tilt servos  
+      shieldPin = pixy.setServos(panLoop.m_command, tiltLoop.m_command);
+      servoShield.setPWM(0, 0, shieldPin);
    
 #if 0 // for debugging
     sprintf(buf, "%ld %ld %ld %ld", rotateLoop.m_command, translateLoop.m_command, left, right);
     Serial.println(buf);  
-#endif
-  
-  }} 
-
+#endif  
+    } 
+  } 
   else // no object detected, go into reset state
   {
     panLoop.reset();
     tiltLoop.reset();
-    pixy.setServos(panLoop.m_command, tiltLoop.m_command);
+    shieldPin = pixy.setServos(panLoop.m_command, tiltLoop.m_command);
+    servoShield.setPWM(0, 0, shieldPin);
   }
-  
- 
 }
